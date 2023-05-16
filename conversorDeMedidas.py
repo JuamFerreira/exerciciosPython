4c9aea85f5c581c4eba15f825aa32102fd666bdd
JuamFerreira <juamferreira.c@gmail.com>
2023-05-16 16:10:06 -0300

    [EXE-01] - Primeira versão do conversor de medidas

--- conversorDeMedidas.py
+++ conversorDeMedidas.py
@@ -1,1 +1,10 @@
 # Converte valor em metro para outras medidas possíveis.
+medida = float(input("digite a medida em metros: "))
+
+print(f'A medida de { medida }m corresponde a: \n'
+      f'{ medida / 1000 }km \n'
+      f'{ medida / 100 }hm \n'
+      f'{ medida / 10 }dam \n'
+      f'{ medida * 10 }dm \n'
+      f'{ medida * 100 }cm \n'
+      f'{ medida * 1000 }mm')
