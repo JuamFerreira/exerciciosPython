8571fbe87450f865161f4d61e8cf4b3c4120ba51
JuamFerreira <juamferreira.c@gmail.com>
2023-05-22 16:58:39 -0300

    [EXE-02] Envio do Calculo Por Área

---
+++ calculoPorArea.py
@@ -0,0 +1,6 @@
+altura = float(input('Digite o valor em m da altura: '))
+largura = float(input('Digite o valor da m largura: '))
+
+print(f'A área total em entre { altura }mX{ largura }m, é { altura*largura }m² \n')
+
+print(f'Será necessário { (altura * largura) / 2 }l de tinta para pintar essa área')
