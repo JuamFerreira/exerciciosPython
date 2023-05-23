578fcae29f85b5481963be455947c5026673c75c
JuamFerreira <juamferreira.c@gmail.com>
2023-05-23 18:12:42 -0300

    [EXE-05] - Uso simples de importação

---
+++ extrairInteiro.py
@@ -0,0 +1,8 @@
+from math import trunc
+
+numero = float(input('Digite um valor: '))
+print(f'O valor digitado foi {numero} e sua parte inteira é {trunc(numero)}')
+
+# Sem necessidade de importação
+num = float(input('Digite um valor: '))
+print(f'O valor digitado foi {num} e sua parte inteira é {int(num)}')
