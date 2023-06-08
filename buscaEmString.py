a4eda92f4b4665fe405add6c347ae33a9eb2b2bb
JuamFerreira <juamferreira.c@gmail.com>
2023-06-08 15:40:42 -0300

    [EXE-13 and 14] - Otimizando a execução dos exercícios.

--- buscaEmString.py
+++ buscaEmString.py
@@ -1,13 +1,11 @@
 """ Realiza alguas buscas dentro de uma string """
 
-frase = input('Digite uma frase: ').strip().lower()
+frase = input('Digite seu nome completo: ').strip()
 
 print(f'A letra A aparece {frase.lower().count("a")} vezes.')
-print(f'A primeira vez que a letra A aparece é na posição {frase.find("a")+1}.')
-print(f'A última vez que a letra A aparece é na posição {frase.rfind("a")+1}.')
+print(f'A primeira vez que a letra A aparece é na posição {frase.lower().find("a") + 1}.')
+print(f'A última vez que a letra A aparece é na posição {frase.lower().rfind("a") + 1}.\n')
 
-print('\n')
-
-nome = input('Agora digite seu nome completo: ').strip().split()
+nome = frase.split()
 print(f'Seu primeiro nome é {nome[0]}.')
 print(f'Seu último nome é {nome[len(nome) - 1]}.')
