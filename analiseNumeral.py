a5529e561616a36a7ffca1e406e91973c8afa164
JuamFerreira <juamferreira.c@gmail.com>
2023-06-08 02:12:26 -0300

    [EXE-11-1] - Analisando numerais matematicamente

--- analiseNumeral.py
+++ analiseNumeral.py
@@ -1,13 +1,15 @@
 """ Analisa as posições de um numeral e retorna sua classe """
 
-numero = input('Digite um numeral com 4 algarismos: ')
+numero = int(input('Digite um numeral com até 6 algarismos: '))
 
 print(f'{" "*6}Analisando o numeral {numero}')
 
-if len(numero) == 4:
-    print(f'Unidade: {numero[3]}')
-    print(f'Dezena: {numero[2]}')
-    print(f'Centena: {numero[1]}')
-    print(f'Unidade de Milhar: {numero[0]}')
+if len(str(numero)) <= 6:
+    print(f'Unidade:            {numero // 1 % 10}')
+    print(f'Dezena:             {numero // 10 % 10}')
+    print(f'Centena:            {numero // 100 % 10}')
+    print(f'Unidade de Milhar:  {numero // 1000 % 10}')
+    print(f'Dezena de Milhar:   {numero // 10000 % 10}')
+    print(f'Centena de Milhar:  {numero // 100000 % 10}')
 else:
     print("Número fora do tamanho esperado.")
