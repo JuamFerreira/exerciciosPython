e78fcad5140760d079d59dd72ef58e266c437f75
JuamFerreira <juamferreira.c@gmail.com>
2023-06-08 15:34:19 -0300

    [EXE-14] - Retornando primeiro e último nome de uma pessoa

--- buscaEmString.py
+++ buscaEmString.py
@@ -5,3 +5,9 @@ frase = input('Digite uma frase: ').strip().lower()
 print(f'A letra A aparece {frase.lower().count("a")} vezes.')
 print(f'A primeira vez que a letra A aparece é na posição {frase.find("a")+1}.')
 print(f'A última vez que a letra A aparece é na posição {frase.rfind("a")+1}.')
+
+print('\n')
+
+nome = input('Agora digite seu nome completo: ').strip().split()
+print(f'Seu primeiro nome é {nome[0]}.')
+print(f'Seu último nome é {nome[len(nome) - 1]}.')
