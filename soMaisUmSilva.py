142cc84f3cff8ab0ad0213c47ac98b13604dcf87
JuamFerreira <juamferreira.c@gmail.com>
2023-06-08 02:46:45 -0300

    [EXE-12] - Buscando palavras em uma string

---
+++ soMaisUmSilva.py
@@ -0,0 +1,8 @@
+""" Programa que lê um nome e verifica se é um Silva """
+
+nome = input('Digite seu nome completo: ')
+
+if 'silva' in nome.lower():
+    print('Sim, você é só mais um Silva')
+else:
+    print('Nem Silva tu é')
