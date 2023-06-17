574ee7305006d58530acdce588e57ee8ccf634ee
JuamFerreira <juamferreira.c@gmail.com>
2023-06-17 13:01:37 -0300

    [EXE-16] Cálculo de multa por velocidade.

---
+++ limiteVelocidade.py
@@ -0,0 +1,10 @@
+""" Informa o valor da multa por km excedido """
+
+velocidade = float(input('Digite a velocidade do veículo: '))
+
+if velocidade > 80:
+	print(f'''\tVocê excedeu o limite de velocidade de 80km/h
+	Por estar na velocidade de {velocidade}km/h pagará a seguinte multa:
+	{(velocidade - 80) * 7 :.2f}R$''')
+else:
+	print('Limite de velocidade respeitado!')
