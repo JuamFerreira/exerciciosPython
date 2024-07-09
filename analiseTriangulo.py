08f5641cc74960cbac8857b4ef0ac0885138758b
Juam Ferreira <juamferreira.c@gmail.com>
2024-07-09 16:02:35 -0300

    [hotfix exe-42] Adição de condições para classificar os triângulos.

--- analiseTriangulo.py
+++ analiseTriangulo.py
@@ -1,4 +1,7 @@
-""" Analisa se 3 seguimentos de reta podem formar um triângulo """
+"""
+	Analisa se 3 seguimentos de reta podem formar um triângulo
+	Adição de condições para classificar os triângulos.
+"""
 
 reta1 = int(input('Digite o primeiro seguimento de reta: '))
 reta2 = int(input('Digite o segundo seguimento de reta: '))
@@ -6,5 +9,11 @@ reta3 = int(input('Digite o terceiro seguimento de reta: '))
 
 if reta1 < reta2 + reta3 and reta2 < reta3 + reta1 and reta3 < reta1 + reta2:
 	print('É POSSÍVEL montar um triângulo com esses seguimentos de reta.')
+	if reta1 == reta2 == reta3:
+		print('Será formado um triâgulo equilátero.')
+	elif reta3 != reta2 != reta1 != reta3:
+		print('Será formado um triâgulo escaleno.')
+	else:
+		print('Será formado um triâgulo Isósceles.')
 else:
 	print('Tamanho das retas INSUFICIENTE para formar um triângulo.')
